f913dae56e0ae6b628024aca9bef25f19803d3f3
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-10 00:00:14 +0300

    Navigate methods have created.

--- src/test/java/practices1/C01.java
+++ src/test/java/practices1/C01.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class C01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -15,7 +15,22 @@ public class C01 {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+        driver.get("https://github.com/idilbusra");
+        Thread.sleep(2000);
 
+       driver.navigate().to("www.google.com");
+        Thread.sleep(2000);
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+        driver.navigate().forward();
+        Thread.sleep(2000);
+
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
+        driver.close();
     }
 
 }
