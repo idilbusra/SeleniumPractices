1729bdf8054c54a187770e0b8c69f05151a7ae4f
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-11 19:17:39 +0300

    Task has added.

---
+++ src/test/java/practices03/C01.java
@@ -0,0 +1,21 @@
+package practices03;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        //siteye git
+        //hesabim butonuna tikla
+        //giris yap buyonuna tikla
+        //uyeol butonuna tikla
+        //isim kutusuna isim gir
+        //soyisim kutusuna soyisim gir
+        //fakemailden mail al
+        //fakemail icin driver olustur
+        //mail kutusuna fakemailden gelen mail adresini yaz
+        //sifre kutusuna sifre gir
+        //kayitol/uyeol butonna tikla
+        //alisverise devam et butonuna tikla
+
+    }
+
+}
