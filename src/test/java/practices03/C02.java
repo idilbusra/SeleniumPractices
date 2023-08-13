b7bdb076cbc9aff39e4c0c9564090917b159ebde
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-13 23:26:27 +0300

    Task has added.

---
+++ src/test/java/practices03/C02.java
@@ -0,0 +1,22 @@
+package practices03;
+
+
+public class C02 {
+    /*
+   1-driver olusturalim
+   2-Java class'imiza chromedriver.exe'yi tanitalim
+   3-driver'in tum ekrani kaplamasini saglayalim
+   4-driver'a sayfanın yuklenmesini 10.000 milisaniye (10 saniye) boyunca beklesini
+  söyleyelim. Eger oncesinde sayfa yuklenirse, beklemeyi bıraksin.
+   5-"sahibinden.com" adresine gidelim
+   6-Bu web adresinin sayfa basligini(title) ve adres(url)ini alalim
+   7-Title ve Url'nin "Kiralik" kelimesinin icerip icermedigini kontrol edelim
+   8-Ardindan "gittigidiyor.com" adresine gidelim
+   9-bu adresin basligini alalim ve "alisveris" kelimesini icerip icermedigini
+  kontrol edelim
+   10-Bir onceki web sayfamiza geri donelim
+   11-Sayfayi yenileyelim
+   12-Daha sonra web sayfamiza tekrar donelim ve oldugumuz sayfayi kapatalim
+   13-Son adim olarak butun sayfalarimizi kapatmis olalim
+     */
+}
