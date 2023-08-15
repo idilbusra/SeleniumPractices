fb99144faab6f69f4c4e98f58f91f19acd6710cf
busra idil <diyetisyenbusraidil@gmail.com>
2023-08-15 23:54:52 +0300

    C03 Class has created and steps has added.

---
+++ src/test/java/practices03/C03.java
@@ -0,0 +1,30 @@
+package practices03;
+
+public class C03 {
+
+    /*
+    Task Steps
+    1.siteye git
+    2.siteye gittigini kontrol et
+    3.hesap olustur butonuna tikla
+    4.hesap olustur butonuna tikla
+    5.Tekrar hesap olustur butonuna tikla
+    6.Adiniz bolumune "Busra" gir
+    7.Soyadiniz bolumune "Surname" gir.
+    8.FakeMail'den fake mail al
+    9.fakemail adresine git
+    10.email kutusuna git email adresini al
+    11.email adresin fakeMailden alinan adresi gir
+    12.Ceptelefonu bilgilerini gir
+    13.Önemli kampanyalardan eposta ile haberdar olmak istiyorum, kutusuna tikla
+    14.Sifre kutusuna sifre kurallarina uygun sifre gir
+    Şifrenizde 8 veya daha fazla karakter kullanın.
+    Şifrenizin en az bir harf içermesine dikkat edin.
+    Şifrenizin en az bir sayı içermesine dikkat edin.
+
+    15.Sifre tekrar kutusuna ayni sifreyi tekrar gir
+    16.Hesabimi olustur butonuna tikla
+    17.Mevcut bilgilerle giris yap
+ */
+
+}
