bb28564bd5c69d6fdd0974e3b3cc3531f25b83b5
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-13 21:00:05 +0300

    TC01 class created and Test Steps have added.

---
+++ src/test/java/ssg/TC01.java
@@ -0,0 +1,20 @@
+package ssg;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class TC01 extends TestBase {
+    @Test
+    public void locatorsTest() {
+        //Test Görevi:
+        //https://www.kitapyurdu.com/ adresine gidiniz.
+        //“En Çok Satılanlar” bölümündeki ilk ürünü tıklayın.
+        //Seçilen ürüne ait sayfanın açıldığını doğrulayınız.
+        //“Devamını Oku…” butonuna basınız.
+        //Ürünü sepete ekleyiniz.
+        //Sepete gidiniz.
+        //Ürüne ait kontrol kutusunu işaretleyiniz.
+        //Ürün miktarını değiştiriniz.
+
+    }
+}
