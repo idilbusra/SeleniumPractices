d5b8098adaea90e9b52722eb145bf95a8e4df6f9
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-14 22:30:47 +0300

    TC02-V2

--- src/test/java/ssg/TC02.java
+++ src/test/java/ssg/TC02.java
@@ -1,7 +1,15 @@
 package ssg;
 
 
-public class TC02 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class TC02 extends TestBase {
     /*
         https://www.kitapyurdu.com/ adresine gidiniz.
         Anasayfanın açıldığını sayfa URL'si ile doğrulayınız.
@@ -14,7 +22,55 @@ public class TC02 {
         Haftanın Yayınevi bölümündeki kitap resimlerinin genişliklerinin 120 olduğunu doğrulayınız.
     */
 
+    @Test
+    public void getMetotlariTesti() {
+        // Kitap Yurdu sitesine gidilir
+        driver.get("https://www.kitapyurdu.com/");
+
+        // URL ile doğrulama yapılır
+        String expectedUrl = "https://www.kitapyurdu.com/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
+
+        // Sayfa başlığı ile doğrulama yapılır
+        String expectedTitle = "Kitapyurdu, Kitapla buluşmanın en kolay yolu";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+
+        // Anasayfada "Haftanın Yayınevi", "En Çok Satılanlar" ve "Ayın Yayınevleri" başlıklarının bulunduğu doğrulanır
+        String pageSource = driver.getPageSource();
+        String expectedText = "Haftanın Yayınevi";
+        Assert.assertTrue(pageSource.contains(expectedText));
+
+        expectedText = "En Çok Satılanlar";
+        Assert.assertTrue(pageSource.contains(expectedText));
+
+        expectedText = "Ayın Yayınevleri";
+        Assert.assertTrue(pageSource.contains(expectedText));
+
+        // Üye girişi butonunun üzerindeki metnin "Giriş Yap" olduğu doğrulanır
+        WebElement girisButonu = driver.findElement(By.xpath("//div[@class='menu-top-button login']"));
+        String girisButonuMetni = girisButonu.getText();
+        expectedText = "Giriş Yap";
+        Assert.assertEquals(expectedText, girisButonuMetni);
+
+        // "Üye Ol" butonunun linkinin "https://www.kitapyurdu.com/index.php?route=account/register" olduğu doğrulanır
+        WebElement uyeOlButonu = driver.findElement(By.linkText("Üye Ol"));
+        String uyeOlButonuHref = uyeOlButonu.getAttribute("href");
+        expectedText = "https://www.kitapyurdu.com/index.php?route=account/register";
+        Assert.assertEquals(expectedText, uyeOlButonuHref);
+
+        // En Çok Satılanlar bölümündeki yazar isimlerinin hepsinin span tagı içerisinde olduğu doğrulanır
+        List<WebElement> yazarIsmiListesi = driver.findElements(By.xpath("//span[@class='author line-clamp-2']"));
+        for (WebElement yazarIsmi : yazarIsmiListesi){
+            Assert.assertEquals("span", yazarIsmi.getTagName());
+        }
+
+
+
+    }
 }
+
 
 
 
