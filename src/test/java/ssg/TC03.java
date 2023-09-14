7904a318d60e807906adddbdd1016a3717cbbbeb
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-14 22:35:11 +0300

    TC03 class has created and  test steps have added.

---
+++ src/test/java/ssg/TC03.java
@@ -0,0 +1,18 @@
+package ssg;
+
+public class TC03 {
+    /*
+     https://www.kitapyurdu.com/ adresine gidiniz.
+Arama motorunda “Java” kelimesini aratınız.
+Listedeki birinci ve ikinci ürünü sepete ekleyiniz.
+Sepetinize gidiniz.
+Eklenen iki ürünün de sepette göründüğünü doğrulayınız.
+İlk ürünün miktarını 2, ikinci ürünün miktarını 3 olarak değiştiriniz.
+“Seçilenleri Daha Sonra Al” fonksiyonunun görünür olmadığını doğrulayınız.
+Ürünlere ait kontrol kutularını seçili hale getiriniz.
+Kontrol kutularının seçili olduğunu doğrulayınız.
+İkinci ürüne ait kontrol kutusunun seçimini kaldırınız.
+İkinci ürünün kontrol kutusunun seçili olmadığını doğrulayınız.
+“Seçilenleri Daha Sonra Al” fonksiyonunun aktif olduğunu doğrulayınız.
+     */
+}
