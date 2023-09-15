3494b688e9337499467b1366822194d17e252a62
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-15 13:43:41 +0300

    AssertionHazirlik class has created and test cases has added.

---
+++ src/test/java/ssg/AssertionHazirlik.java
@@ -0,0 +1,39 @@
+package ssg;
+
+import org.junit.Test;
+
+public class AssertionHazirlik {
+    int ehliyetYasi = 18;
+    int alininYasi = 15;
+    int ayseninYasi = 23;
+
+    @Test
+    public void test01() {
+        // Ali'nin ehliyet alabileceğini test edin.
+
+    }
+
+    @Test
+    public void test02() {
+        // Ayşe'nin ehliyet alamayacağını test edin
+
+    }
+
+    @Test
+    public void test03() {
+        // Ehliyet yaşının 18 olduğunu test edin
+
+    }
+
+    @Test
+    public void test04() {
+        // Ali'nin yaşının Ayşe'nin yaşından büyük olduğunu test edin
+
+    }
+
+    @Test
+    public void test05() {
+        // Ali'nin yaşının Ayşe'nin yaşından büyük olmadığını test edin
+
+    }
+}
