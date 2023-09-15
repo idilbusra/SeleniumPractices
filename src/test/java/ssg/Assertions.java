57fc281bc4dc9a0160538b04f56f266260a7ad94
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-15 13:53:02 +0300

    Assertions class has created and test steps has added.

---
+++ src/test/java/ssg/Assertions.java
@@ -0,0 +1,13 @@
+package ssg;
+
+public class Assertions {
+     /*
+    Test Görevi:
+        https://www.kitapyurdu.com/ adresine gidiniz.
+        Anasayfanın açıldığını doğrulayınız (Url, Title, Logo)
+        Arama motorunda "java" kelimesini aratınız.
+        Ürün listesinin dolu olduğunu doğrulayınız.
+        Arama sonuçlarının "java" kelimesini içerdiğini doğrulayınız.
+        Arama sonuçlarının "javascript" kelimesini içermediğini doğrulayınız.
+    */
+}
