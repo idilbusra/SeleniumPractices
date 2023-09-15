6c181bf5319a0f724bd228a25e3736d05e5f2bdd
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-15 13:45:39 +0300

    AssertionHazirlik-V2

--- src/test/java/ssg/AssertionHazirlik.java
+++ src/test/java/ssg/AssertionHazirlik.java
@@ -1,5 +1,6 @@
 package ssg;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AssertionHazirlik {
@@ -10,30 +11,36 @@ public class AssertionHazirlik {
     @Test
     public void test01() {
         // Ali'nin ehliyet alabileceğini test edin.
-
+        Assert.assertTrue("Bu kişi ehliyet alamaz!", alininYasi >= ehliyetYasi);
+        //         True                     False             == FAIL
     }
 
     @Test
     public void test02() {
         // Ayşe'nin ehliyet alamayacağını test edin
+        Assert.assertFalse("Bu kişi ehliyet alabilir!", ayseninYasi >= ehliyetYasi);
+        //         False                       True            == FAIL
 
     }
 
     @Test
     public void test03() {
         // Ehliyet yaşının 18 olduğunu test edin
-
+        Assert.assertEquals(18, ehliyetYasi);
+        //           True              True          == PASS
     }
 
     @Test
     public void test04() {
         // Ali'nin yaşının Ayşe'nin yaşından büyük olduğunu test edin
-
+        Assert.assertTrue(alininYasi > ayseninYasi);
+        //         True                     False           == FAIL
     }
 
     @Test
     public void test05() {
         // Ali'nin yaşının Ayşe'nin yaşından büyük olmadığını test edin
-
+        Assert.assertFalse(alininYasi > ayseninYasi);
+        //         False                     False           == PASS
     }
 }
