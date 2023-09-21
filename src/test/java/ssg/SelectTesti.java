bc598a92d128a36b39459d20a711f8c673f9149e
busra idil <diyetisyenbusraidil@gmail.com>
2023-09-21 21:45:10 +0300

    SelectTesti-v2

--- src/test/java/ssg/SelectTesti.java
+++ src/test/java/ssg/SelectTesti.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
+import java.util.List;
+
 public class SelectTesti extends TestBase {
     /*
     Test Görevi - 1:
@@ -36,5 +38,41 @@ public class SelectTesti extends TestBase {
 
         // Menüde seçilen seçeneği yazdır
         System.out.println("Menüde Seçili Olan Seçenek: " + apple);
+    }
+
+    @Test
+    public void dropDownTesti02() {
+        /*
+        Test Görevi - 2:
+            letcode.in/dropdowns adresine gidin.
+            Programlama Dilleri menüsünün tıklanabilir olduğunu doğrulayın.
+            Menüdeki son seçeneği seçin.
+            Seçilen seçeneğin C# olduğunu doğrulayın.
+            Listede C++ seçeneğinin olmadığını doğrulayın.
+            Menüdeki tüm elemanları yazdırın.
+        */
+
+        // LetCode sitesine git
+        driver.get("https://letcode.in/dropdowns");
+
+        // Programlama Dilleri menüsünün tıklanabilir olduğunu doğrula
+        WebElement programlamaDilleri = driver.findElement(By.id("lang"));
+        Assert.assertTrue(programlamaDilleri.isEnabled());
+
+        // Menüdeki son seçeneği seç
+        Select select = new Select(programlamaDilleri);
+        List<WebElement> programlamaDilleriListesi = select.getOptions();
+        select.selectByIndex(programlamaDilleriListesi.size() - 1);
+        select.selectByVisibleText(programlamaDilleriListesi.get(programlamaDilleriListesi.size() - 1).getText());
+
+        // Seçilen seçeneğin C# olduğunu doğrulayın.
+        Assert.assertEquals("C#", select.getFirstSelectedOption().getText());
+
+        // Listede C++ seçeneğinin olmadığını doğrula
+        // Menüdeki tüm elemanları yazdır
+        for (WebElement programlamaDili : programlamaDilleriListesi) {
+            Assert.assertNotEquals("C++", programlamaDili.getText());
+            System.out.println(programlamaDili.getText());
+        }
     }
 }
